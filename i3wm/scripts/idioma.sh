841b235febab5be84e7b60694b192e1357f65060
alejandro <alebrenes316@gmail.com>
2024-06-16 23:49:35 -0600

    Update and rename i3modules/idioma.sh to i3wm/scripts/idioma.sh

--- i3wm/scripts/idioma.sh
+++ i3wm/scripts/idioma.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/bash
 
-# Colores
-blanco="#FFFFFF"
-
 idioma=$(setxkbmap -query | awk '/layout/{print $2}')
 
 if [[ "$idioma" == "es" ]]; then
 	setxkbmap us
-	echo "<span color='$blanco'>us</span>"
 elif [[ "$idioma" == "us" ]]; then
 	setxkbmap es
-	echo "<span color='$blanco'>es</span>"
-
 fi
